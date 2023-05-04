01ad06496a4d6fed26149c34b9e92f633132ca97
Franco Olmedo <francoolmedo424@gmail.com>
2023-05-04 17:49:16 -0300

    Completando insercion y busqueda

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,127 @@
 #include "pa2m.h"
+#include "src/abb.h"
+#include "src/abb_estructura_privada.h"
 
-void prueba_simple()
+int comparador(void *elemento_1, void *elemento_2)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	int *e1 = elemento_1;
+	int *e2 = elemento_2;
+
+	return (*e1) - (*e2);
+}
+
+void prueba_creacion_arbol()
+{
+	abb_t *arbol_NULL = abb_crear(NULL);
+	abb_t *arbol = abb_crear(comparador);
+	pa2m_afirmar(
+		arbol_NULL == NULL,
+		"No se puede crear un arbol con función nula (abb_crear devuelve NULL)");
+	pa2m_afirmar(
+		arbol != NULL,
+		"El arbol creado no devuelve NULL, se pudo crear el arbol");
+	pa2m_afirmar(
+		arbol->comparador != NULL,
+		"El arbol creado posee una funcion comparadora (el puntero comparador no devuelve NULL)");
+	pa2m_afirmar(arbol->tamanio == 0,
+		     "El arbol está vacío, su tamaño es 0");
+}
+
+void prueba_insercion_en_arbol()
+{
+	pa2m_afirmar(abb_insertar(NULL, NULL) == NULL,
+		     "Intentar insertar en arbol nulo devuelve NULL");
+	abb_t *abb = abb_crear(comparador);
+	int elemento = 23;
+	int elemento_2 = 15;
+	int elemento_3 = 37;
+	abb_insertar(abb, &elemento);
+	pa2m_afirmar(abb->comparador(&elemento, abb->nodo_raiz->elemento) == 0,
+		     "Se insertó un elemento en nodo raiz");
+	pa2m_afirmar(abb->tamanio == 1, "El tamaño del arbol subió a 1");
+	pa2m_afirmar(abb->nodo_raiz->derecha == NULL,
+		     "Puntero al nodo derecha de la raíz devuelve NULL");
+	pa2m_afirmar(abb->nodo_raiz->izquierda == NULL,
+		     "Puntero al nodo izquierda de la raíz devuelve NULL");
+	abb_insertar(abb, &elemento_2);
+	pa2m_afirmar(
+		abb->tamanio == 2,
+		"Se insertó un elemento menor, el tamaño del arbol subió a 2");
+	abb_insertar(abb, &elemento_3);
+	pa2m_afirmar(
+		abb->tamanio == 3,
+		"Se insertó un elemento mayor, el tamaño del arbol subió a 3");
+	pa2m_afirmar(
+		abb->nodo_raiz->derecha != NULL,
+		"Puntero al nodo derecha de la raíz no devuelve NULL, se insertó el nodo");
+	pa2m_afirmar(
+		abb->nodo_raiz->izquierda != NULL,
+		"Puntero al nodo izquierda de la raíz no devuelve NULL, se insertó el nodo");
+}
+
+void prueba_busqueda_en_arbol()
+{
+	abb_t *abb = abb_crear(comparador);
+	abb_t *abb_vacio = abb_crear(comparador);
+	int elemento = 23;
+	int elemento_b = 23;
+	int elemento_2 = 15;
+	int elemento_3 = 37;
+	int elemento_4 = 40;
+	int elemento_5 = 12;
+	int elemento_6 = 16;
+	int elemento_7 = 32;
+	abb_insertar(abb, &elemento);
+	void *elemento_encontrado = abb_buscar(NULL, &elemento_b);
+	pa2m_afirmar(
+		elemento_encontrado == NULL,
+		"No se puede buscar un elemento en un arbol nulo, devuelve NULL");
+	elemento_encontrado = abb_buscar(abb_vacio, &elemento_b);
+	pa2m_afirmar(
+		elemento_encontrado == NULL,
+		"No se puede buscar un elemento en un arbol vacio, devuelve NULL");
+	elemento_encontrado = abb_buscar(abb, &elemento_b);
+	pa2m_afirmar(comparador(elemento_encontrado, &elemento_b) == 0,
+		     "Se encontró el elemento en la raíz");
+	elemento_encontrado = abb_buscar(abb, &elemento_2);
+	pa2m_afirmar(elemento_encontrado == NULL,
+		     "Si se busca un elemento inexistente se devuelve NULL");
+	abb_insertar(abb, &elemento_2);
+	abb_insertar(abb, &elemento_3);
+	abb_insertar(abb, &elemento_4);
+	abb_insertar(abb, &elemento_5);
+	abb_insertar(abb, &elemento_6);
+	abb_insertar(abb, &elemento_7);
+	elemento_encontrado = abb_buscar(abb, &elemento_2);
+	pa2m_afirmar(comparador(elemento_encontrado, &elemento_2) == 0,
+		     "Se encontró el elemento de la izquierda de la raíz");
+	elemento_encontrado = abb_buscar(abb, &elemento_3);
+	pa2m_afirmar(comparador(elemento_encontrado, &elemento_3) == 0,
+		     "Se encontró el elemento de la derecha de la raíz");
+	elemento_encontrado = abb_buscar(abb, &elemento_4);
+	pa2m_afirmar(comparador(elemento_encontrado, &elemento_4) == 0,
+		     "Se encontró el elemento del subarbol derecho");
+	elemento_encontrado = abb_buscar(abb, &elemento_5);
+	pa2m_afirmar(comparador(elemento_encontrado, &elemento_5) == 0,
+		     "Se encontró el elemento del subarbol izquierdo");
+	elemento_encontrado = abb_buscar(abb, &elemento_6);
+	pa2m_afirmar(
+		comparador(elemento_encontrado, &elemento_6) == 0,
+		"Se encontró el elemento del subarbol izquierdo, predecesor inorden a raíz");
+	elemento_encontrado = abb_buscar(abb, &elemento_7);
+	pa2m_afirmar(
+		comparador(elemento_encontrado, &elemento_7) == 0,
+		"Se encontró el elemento del subarbol derecho, sucesor inorden a raiz");
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	pa2m_nuevo_grupo("\nPruebas creación de arbol");
+	prueba_creacion_arbol();
+	pa2m_nuevo_grupo("\nPruebas insercion en arbol");
+	prueba_insercion_en_arbol();
+	pa2m_nuevo_grupo("\nPruebas busqueda en arbol");
+	prueba_busqueda_en_arbol();
 
 	return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -2,14 +2,51 @@
 #include "abb_estructura_privada.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 abb_t *abb_crear(abb_comparador comparador)
 {
-	return NULL;
+	if (comparador == NULL)
+		return NULL;
+	abb_t *arbol = calloc(1, sizeof(abb_t));
+	arbol->comparador = comparador;
+	return arbol;
+}
+
+nodo_abb_t *abb_insertar_recursivo(nodo_abb_t *raiz, void *elemento,
+				   abb_comparador comparador, bool *error)
+{
+	if (raiz == NULL) {
+		nodo_abb_t *nodo_insertar = malloc(sizeof(nodo_abb_t));
+		if (nodo_insertar == NULL) {
+			*error = true;
+			return NULL;
+		}
+		nodo_insertar->elemento = elemento;
+		nodo_insertar->derecha = NULL;
+		nodo_insertar->izquierda = NULL;
+		return nodo_insertar;
+	}
+
+	if (comparador(raiz->elemento, elemento) >= 0)
+		raiz->izquierda = abb_insertar_recursivo(
+			raiz->izquierda, elemento, comparador, error);
+	if (comparador(raiz->elemento, elemento) < 0)
+		raiz->derecha = abb_insertar_recursivo(raiz->derecha, elemento,
+						       comparador, error);
+	return raiz;
 }
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
+	if (arbol == NULL)
+		return NULL;
+	bool error = false;
+	arbol->nodo_raiz = abb_insertar_recursivo(arbol->nodo_raiz, elemento,
+						  arbol->comparador, &error);
+	if (error)
+		return NULL;
+	arbol->tamanio++;
 	return arbol;
 }
 
@@ -18,23 +55,52 @@ void *abb_quitar(abb_t *arbol, void *elemento)
 	return elemento;
 }
 
+void *abb_buscar_recursivo(nodo_abb_t *raiz, void *elemento,
+			   abb_comparador comparador, void *elemento_encontrado)
+{
+	if (raiz == NULL)
+		return NULL;
+	if (comparador(raiz->elemento, elemento) == 0)
+		return raiz->elemento;
+	if (comparador(raiz->elemento, elemento) >= 0)
+		elemento_encontrado = abb_buscar_recursivo(raiz->izquierda,
+							   elemento, comparador,
+							   elemento_encontrado);
+	if (comparador(raiz->elemento, elemento) < 0)
+		elemento_encontrado = abb_buscar_recursivo(raiz->derecha,
+							   elemento, comparador,
+							   elemento_encontrado);
+	return elemento_encontrado;
+}
+
 void *abb_buscar(abb_t *arbol, void *elemento)
 {
-	return elemento;
+	void *elemento_encontrado = NULL;
+	if (abb_vacio(arbol))
+		return NULL;
+	elemento_encontrado = abb_buscar_recursivo(arbol->nodo_raiz, elemento,
+						   arbol->comparador,
+						   elemento_encontrado);
+	return elemento_encontrado;
 }
 
 bool abb_vacio(abb_t *arbol)
 {
-	return true;
+	if (arbol == NULL || arbol->tamanio == 0)
+		return true;
+	return false;
 }
 
 size_t abb_tamanio(abb_t *arbol)
 {
-	return 0;
+	if (arbol == NULL)
+		return 0;
+	return arbol->tamanio;
 }
 
 void abb_destruir(abb_t *arbol)
 {
+	free(arbol);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
